fbdef8613f59786c1cb391d3e3870c9ed9bc8111
CharlesCampisi <campisi.charles@gmail.com>
2019-10-12 11:47:20 -0400

    database1

---
+++ Database/DVOASchema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE DVOA_data (
+	Team VARCHAR,
+Year int,
+	Week int,
+	Opponent VARCHAR,
+	TotalDVOA DEC,
+	OffenseDVOA DEC,
+	OffPassDVOA DEC,
+	OffRushDVOA DEC,
+	DefenseDVOA DEC,
+	DefPassDVOA DEC,
+	DefRushDVOA DEC,
+	STDVOA DEC
+);
---
+++ Database/GamblingSchema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE gambling_data (
+	Year int,
+	Week int,
+	HomeTeam VARCHAR,
+	AwayTeam VARCHAR,
+	HomeScore int,
+	AwayScore int,
+	PointDiff DEC,
+	HomeLine DEC,
+	AwayLine DEC,
+	PredictTotal DEC,
+	ActualTotal	int
+);
