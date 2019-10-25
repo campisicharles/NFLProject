3eceacf7c5f27a7a470cc56240a9fd35ec267156
crescenzo77 <crescenzo@gmail.com>
2019-10-25 11:00:25 -0400

    latest updates for week 8

---
+++ Database/OutcomesSchema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE outcomes_data (
+Home VARCHAR,
+Away VARCHAR,
+Year int,
+Week int,
+ActualDifferential int,
+ActualTotal int,
+WinnerBetWon int,
+SpreadBetWon int,
+OverUnderBetWon int
+);
